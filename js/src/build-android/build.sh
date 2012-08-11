880164fc87bb407afc34663e4f682fa023bcad3b
folecr <folecr@gmail.com>
2012-08-11 01:23:24 -0700

    Update build script to generate release and develop builds.

--- js/src/build-android/build.sh
+++ js/src/build-android/build.sh
@@ -1,9 +1,40 @@
-#!/bin/sh
+# options
+develop=
+release=
+
+usage(){
+cat << EOF
+usage: $0 [options]
+
+Build SpiderMonkey using Android NDK
+
+OPTIONS:
+-d	Build for development
+-r  Build for release
+-h	this help
+
+EOF
+}
+
+while getopts "drh" OPTION; do
+case "$OPTION" in
+d)
+develop=1
+;;
+r)
+release=1
+;;
+h)
+usage
+exit 0
+;;
+esac
+done
 
 # configure
 ../configure --with-android-ndk=$HOME/bin/android-ndk \
              --with-android-sdk=$HOME/bin/android-sdk \
-             --with-android-version=5 \
+             --with-android-version=14 \
              --with-android-toolchain=$HOME/bin/android-ndk/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86 \
              --enable-application=mobile/android \
              --target=arm-linux-androideabi \
@@ -19,12 +50,20 @@
 # make
 make -j4
 
+if [[ $develop ]]; then
+    rm -rf ../../../dist
+    ln -s -f "$PWD"/dist -t ../../..
+fi
+
+if [[ $release ]]; then
 # copy specific files from dist
-mkdir -p ../../../dist
-mkdir -p ../../../dist/include
-cp -RL dist/include/* ../../../dist/include/
-mkdir -p ../../../dist/lib
-cp -RL dist/lib/libjs_static.a ../../../dist/lib/libjs_static.a
+    rm -rf ../../../dist
+    mkdir -p ../../../dist
+    mkdir -p ../../../dist/include
+    cp -RL dist/include/* ../../../dist/include/
+    mkdir -p ../../../dist/lib
+    cp -RL dist/lib/libjs_static.a ../../../dist/lib/libjs_static.a
 
 # strip unneeded symbols
-$HOME/bin/android-ndk/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86/bin/arm-linux-androideabi-strip --strip-unneeded ../../../dist/lib/libjs_static.a
+    $HOME/bin/android-ndk/toolchains/arm-linux-androideabi-4.6/prebuilt/linux-x86/bin/arm-linux-androideabi-strip --strip-unneeded ../../../dist/lib/libjs_static.a
+fi
